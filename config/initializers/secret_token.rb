1909851bd21bd8b2d8023b14576f0ac156cbba85
Timo Hallikainen <triq001 [] gmail.com>
2013-04-25 23:10:25 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TriqSite::Application.config.secret_token = '68035e8880d1cf9b07933d50b0f368049c9e0f6e40e0891ca8f020c9ba8a8bd0724742c694aabcc72d54ee9b0be5d5dda4cf08c532c95b7c2835fe4c0d43fb48'
